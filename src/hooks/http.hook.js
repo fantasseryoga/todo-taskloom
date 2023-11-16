84dc56abc0f7aa5eabb1f56d57d688a247226505
fantasseryoga <69531863+fantasseryoga@users.noreply.github.com>
2023-11-16 21:28:48 +0200

    feat: add http hook

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,37 @@
+import { useState, useCallback } from 'react'
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+        setLoading(true)
+
+        try {
+            if (body) {
+                body = JSON.stringify(body)
+                headers['Content-Type'] = 'application/json'
+                headers['Accept'] = 'application/json'
+            }
+
+            const response = await fetch(url, { method, body, headers })
+
+            setLoading(false)
+
+            if (response.status === 401) {
+                // logout()
+            }
+
+            return response
+        } catch (e) {
+            setLoading(false)
+            setError(e.message)
+
+            throw e
+        }
+    }, [])
+
+    const clearError = useCallback(() => setError(null))
+
+    return { loading, request, error, clearError }
+}
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,49 @@
-import React from "react"
+import { React } from "react"
 import styles from "../index.css"
 import "../styles/auth.scss"
 import { Navbar } from "../components/Navbar"
+import { useState } from "react"
+import { useHttp } from "../hooks/http.hook"
 
 export const LoginPage = () => {
+    const { loading, request } = useHttp()
+    const [formErrors, setFormErrors] = useState([])
+    const [form, setForm] = useState({
+        username: '',
+        password: ''
+    })
+
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value })
+    }
+
+    const loginHandler = async () => {
+        if (form.username === '' || form.password === '') {
+            setFormErrors(['Invalid Credentials'])
+            return
+        }
+        try {
+            const response = await request("/user-login", "POST", { ...form })
+            if (response.status === 400) {
+                const data = await response.json()
+                setFormErrors(data.errors.map(el => el.msg))
+            }
+
+            if (response.status === 200) {
+                const data = await response.json()
+
+                // login(data.access_token, data.user.name)
+                console.log(data)
+                setFormErrors([])
+            }
+
+        }
+        catch (e) {
+            console.log(e)
+            alert("Sorry sth went wrong, try to reload the page.")
+        }
+    }
+
     return (
         <>
             < Navbar />
@@ -14,18 +54,18 @@ export const LoginPage = () => {
                         <label class="block font-bold login-input-label mb-2" for="username">
                             Username
                         </label>
-                        <input className="w-full login-input py-2" id="username" type="text" placeholder="Type here" />
+                        <input className="w-full login-input py-2" id="username" name="username" onChange={changeHandler} type="text" placeholder="Type here" />
                         <label class="block login-input-label font-bold mb-2 mt-12" for="password">
                             Password
                         </label>
-                        <input className="w-full login-input py-2" id="password" type="password" placeholder="Type here"></input>
+                        <input className="w-full login-input py-2" id="password" name="password" onChange={changeHandler} type="password" placeholder="Type here" />
                     </div>
-                    <button class="font-bold py-2 px-9 px-4 log-in-btn">
+                    <button class="font-bold py-2 px-9 px-4 log-in-btn" onClick={loginHandler}>
                         Log In
                     </button>
                 </div>
             </div>
-            <br/>
+            <br />
         </>
     )
 }
